68d750d8f30113683c16ad8a382e757e344e5c25
Damian Lin <dlin6906@uni.sydney.edu.au>
2021-03-04 13:58:29 +1100

    Cleaning

--- library.py
+++ library.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 def swipe_right(func):
 
     @functools.wraps(func)
-    def swipe_right_wrapper(*args, **kwargs):
+    def swipe_right_wrapper(*args):
         from kivy.uix.screenmanager import SlideTransition, NoTransition
         self = args[0]
         self.manager.transition = SlideTransition(direction='left')
@@ -18,7 +18,7 @@ def swipe_right(func):
 def swipe_left(func):
 
     @functools.wraps(func)
-    def swipe_left_wrapper(*args, **kwargs):
+    def swipe_left_wrapper(*args):
         from kivy.uix.screenmanager import SlideTransition, NoTransition
         self = args[0]
         self.manager.transition = SlideTransition(direction='right')
